6a9f63ba24ea669bd406c4db2fba3974688509d3
Carl <Carlgbt@gmail.com>
2024-10-29 16:24:59 -0700

    files

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main()
-{
-    cout<<"Goodbye World " << endl;
-    cout<<"Hello World " << endl;
-    cout<<"Hello World " << endl;
-    cout<<"Hello World " << endl;
-    cout << "Hello CISP" << endl;
-    return 0;
+int main() {
+  // Create and open a text file
+    ofstream MyFile("filename.txt");
+
+  // Write to the file
+    MyFile << "Files can be tricky, but it is fun enough!";
+
+  // Close the file
+    MyFile.close();
 }
